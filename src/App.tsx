9289ef3790f30299178ee0240584ab99f0676227
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2023-12-31 02:10:12 -0300

    chore: remove BUN main runtime toll, add biome to linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
-  return (
-    <div>
-      <h1>Hello there, to this new project!</h1>
-    </div>
-  )
+	return (
+		<div>
+			<h1>Hello there, to this new project!</h1>
+		</div>
+	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootComponent = document.getElementById("root");
+
+if (!rootComponent) throw new Error("A root component must to be provided");
+
+ReactDOM.createRoot(rootComponent).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
