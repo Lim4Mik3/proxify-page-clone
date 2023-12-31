ad97badbf6b44b178cf304601e47c47064e132de
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2023-12-31 03:12:01 -0300

    chore: add linter command on commit trigger

--- src/pages/Home.spec.tsx
+++ src/pages/Home.spec.tsx
@@ -1,12 +1,12 @@
-import { render } from '@testing-library/react';
-import { Home } from './Home';
+import { render } from "@testing-library/react";
+import { Home } from "./Home";
 
-describe('Home | PAGE SUITE TEST', () => {
-  it('should be able render', () => {
-    const wrapper = render(<Home />);
+describe("Home | PAGE SUITE TEST", () => {
+	it("should be able render", () => {
+		const wrapper = render(<Home />);
 
-    wrapper.debug()
+		wrapper.debug();
 
-    expect(wrapper.getByText(/Hello World!/i)).toBeInTheDocument();
-  })
-})
+		expect(wrapper.getByText(/Hello World!/i)).toBeInTheDocument();
+	});
+});
