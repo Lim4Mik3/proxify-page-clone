657c5e5355139c3fef12847f58775e8b21147970
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2023-12-31 02:24:23 -0300

    chore: add and setup tailwindCss with postCss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 
+import './styles/global.css';
+
 const rootComponent = document.getElementById("root");
 
 if (!rootComponent) throw new Error("A root component must to be provided");
